b94c392b99103a5789ba1a4fbfc35e16e7f4928e
Alex <alex.oastler@gmail.com>
2015-04-09 18:56:01 -0400

    started working on save function still a work in progress

--- js/index.js
+++ js/index.js
@@ -1,6 +1,38 @@
-$.ajax("https://api.github.com/legacy/repos/search/javascript").done(function(data) {
-     var i, repo;
-     $.each(data.repositories, function (i, repo) {
-        $("#allRepos").append("<p><a href='https://github.com/" + repo.username + "/" + repo.name + "'>" + repo.name + "</a><br>"+ repo.description + "&glt;/p>");
-     });
+var db;
+
+$('#mainpage').bind('pageinit', function(event) {
+    console.log("binds page");
+	db = window.openDatabase("repodb","0.1","GitHub Repo Db", 1000);
+    db.transaction(createDb, txError, txSuccess);
 });
+
+function createDb(tx) {
+    tx.executeSql("DROP TABLE IF EXISTS repos");
+    tx.executeSql("CREATE TABLE repos(user,name)");
+}
+
+function txError(error) {
+    console.log(error);
+    console.log("Database error: " + error);
+}
+
+function txSuccess() {
+    console.log("Success");
+}
+
+function saveFave() {
+    db = window.openDatabase('document.getElementById("DescriptionUI").value', 'document.getElementById("DateUI").value' );
+    db.transaction(saveFaveDb, txError, txSuccessFave);
+}
+
+function saveFaveDb(tx) {
+    var owner = getUrlVars().owner;
+    var name = getUrlVars().name;
+
+    tx.executeSql("INSERT INTO repos(user,name) VALUES (?, ?)",[owner,name]);
+}
+
+function txSuccessFave() {
+    console.log("Save success");
+
+}
