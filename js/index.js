8eda2b9c4d2287e3c87843927ac4cbf7135e9eb0
Alex <alex.oastler@gmail.com>
2015-04-13 14:50:39 -0400

    working copy of database but no copy out of command

--- js/index.js
+++ js/index.js
@@ -2,13 +2,13 @@ var db;
 
 $('#mainpage').bind('pageinit', function(event) {
     console.log("binds page");
-	db = window.openDatabase("repodb","0.1","GitHub Repo Db", 1000);
+	db = window.openDatabase("homeworkdb","0.1","GitHub Repo Db", 1000);
     db.transaction(createDb, txError, txSuccess);
 });
 
 function createDb(tx) {
-    tx.executeSql("DROP TABLE IF EXISTS repos");
-    tx.executeSql("CREATE TABLE repos(user,name)");
+    tx.executeSql("DROP TABLE IF EXISTS homework");
+    tx.executeSql("CREATE TABLE homework(user,name)");
 }
 
 function txError(error) {
@@ -21,18 +21,38 @@ function txSuccess() {
 }
 
 function saveFave() {
-    db = window.openDatabase('document.getElementById("DescriptionUI").value', 'document.getElementById("DateUI").value' );
+    db = window.openDatabase("homeworkdb","0.1","GitHub Repo Db", 1000);
+	console.log("opens database");
     db.transaction(saveFaveDb, txError, txSuccessFave);
 }
 
 function saveFaveDb(tx) {
-    var owner = getUrlVars().owner;
-    var name = getUrlVars().name;
+    var owner =  document.getElementById("DescriptionUI").value
+    var name =  document.getElementById("DateUI").value
 
-    tx.executeSql("INSERT INTO repos(user,name) VALUES (?, ?)",[owner,name]);
+    tx.executeSql("INSERT INTO homework(user,name) VALUES (?, ?)",[owner,name]);
 }
 
 function txSuccessFave() {
     console.log("Save success");
+
+}
+
+function checkFave() {
+    db.transaction(checkFaveDb, txError);
+}
+
+function checkFaveDb(tx) {
+    var owner = owner
+    var name = name
+
+    tx.executeSql("SELECT * FROM homework", [],txSuccessCheckFave , txError);
+	//tx.executeSql("SELECT * FROM homework WHERE user = ? AND name = ?",[owner,name],txSuccessCheckFave);
+}
+
+function txSuccessCheckFave(tx,results) {
+    console.log("Read success");
+    console.log(results);
+	var owner = document.getElementById("DescriptionUI").value
 
 }
