e3ca5455e2745fba9168c045acd8021ae516005f
Alex <alex.oastler@gmail.com>
2015-05-19 19:44:13 -0400

    Two new rows added to DB working drop down selects

--- js/index.js
+++ js/index.js
@@ -3,25 +3,29 @@ var db;
 $('#mainpage').bind('pageinit', function(event) {
     console.log("binds page");
 	db = window.openDatabase("homeworkdb","0.1","GitHub Repo Db", 1000);
+	//db = window.openDatabase("assignmentsdb","0.1","GitHub Repo Db", 1000);
     db.transaction(createDb, txError, txSuccess);
 });
 $( "body" ).on( "pagecontainerchange", function( event, ui ) {
-    console.log("homework");
 	readInfo();
 	
 });
 
 function createDb(tx) {
-    tx.executeSql("CREATE TABLE homework(duedate,course,description)");
+    tx.executeSql("CREATE TABLE homework(duedate,course,description,category)");
+//	tx.executeSql("CREATE TABLE assignments(duedate,course,description)");
 }
 function resetData() {
 	db = window.openDatabase("homeworkdb","0.1","GitHub Repo Db", 1000);
+	//db = window.openDatabase("assignmentsdb","0.1","GitHub Repo Db", 1000);
     db.transaction(resetDb, txError, txSuccess);
 	
 	}
 function resetDb(tx) {
 tx.executeSql("DROP TABLE IF EXISTS homework");
-tx.executeSql("CREATE TABLE homework(duedate,course,description)");
+//tx.executeSql("DROP TABLE IF EXISTS assignments");
+tx.executeSql("CREATE TABLE homework(duedate,course,description,category)");
+//tx.executeSql("CREATE TABLE assignments(duedate,course,description)");
 }
 function txError(error) {
     //console.log(error);
@@ -34,6 +38,7 @@ function txSuccess() {
 
 function writeInfo() {
     db = window.openDatabase("homeworkdb","0.1","GitHub Repo Db", 1000);
+	//db = window.openDatabase("assignmentsdb","0.1","GitHub Repo Db", 1000);
 	//console.log("opens database");
     
 	
@@ -48,10 +53,13 @@ function writeInfo() {
 function saveFaveDb(tx) {
     var description =  $("#DescriptionED").val();
     var duedate =  $("#DateED").val();
-	var course = "math";
-	console.log(description);
-	console.log(duedate);
-    tx.executeSql("INSERT INTO homework(duedate,course,description) VALUES (?, ?, ?)",[duedate,course,description]);
+	var course = $("#ClassesED").val();
+	var category = $("#categoryED").val();
+	console.log("description is " + description);
+	console.log("duedate is " + duedate);
+	console.log("course is " + course);
+	console.log("category is " + category);
+    tx.executeSql("INSERT INTO homework(duedate,course,description,category) VALUES (?, ?, ?, ?)",[duedate,course,description,category]);
 }
 
 function txSuccessFave() {
@@ -62,15 +70,15 @@ function txSuccessFave() {
 function readInfo() {
     
     db = window.openDatabase("homeworkdb","0.1","GitHub Repo Db", 1000);
-	db.transaction(checkFaveDb, txError);
+	db.transaction(checkHomework, txError);
 }
 
-function checkFaveDb(tx) {
-    tx.executeSql("SELECT * FROM homework",  [], txSuccessCheckFave , txError);
+function checkHomework(tx) {
+    tx.executeSql("SELECT * FROM homework",  [], txSuccessHomework , txError);
 }
 
 
-function txSuccessCheckFave(tx,results) {
+function txSuccessHomework(tx,results) {
 console.log("Read success");
 	$("#homework-table tbody").empty();
 	row = "";
@@ -80,6 +88,7 @@ console.log("Read success");
 		row += "<td>" + results.rows.item(i)['duedate'] + "</td>";
 		row += "<td>" + results.rows.item(i)['description'] + "</td>";
 		row += "<td>" + results.rows.item(i)['course'] + "</td>";
+		row += "<td>" + results.rows.item(i)['category'] + "</td>";
 		row += "</tr>";
 		
 		$( "#homework-table tbody" ).append( row ); 
